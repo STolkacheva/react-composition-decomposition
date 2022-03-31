d380a99cf28c794a13e0dea7c1c60f931b19685c
s.tolkacheva <s.tolkacheva@systtech.ru>
2022-03-31 10:30:01 +0300

    Декомпозиция интерфейса на компоненты

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./App.css";
+import Decomposition from "./components/Decomposition.js";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Decomposition />
+    </div>
+  );
+}
